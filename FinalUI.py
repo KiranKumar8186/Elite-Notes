48ce4ec866cd9d993239d8965d13a87293a389e2
KIRAN KUMAR BODA <98200001+KiranKumar8186@users.noreply.github.com>
2023-02-17 00:30:56 +0530

    Update FinalUI.py

--- FinalUI.py
+++ FinalUI.py
@@ -114,8 +114,7 @@ if url_type == "youtube":
         if url:          
             right_url = valid_url(url)
             if right_url:  
-                get_audio_from_youtube_url(url)
-                st.audio("audio.mp3")
+               
                 if get_video_duration_from_youtube_url(url) <= MAX_VIDEO_LENGTH: 
                     # Display YouTube video
                     _,col2,_ =st.columns([0.2, 0.25, 0.2])
